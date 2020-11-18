f1dd0bc2b3219d74afd1bda16e764ccfdeef0422
utkarsh <piousutkarsh55@gmail.com>
2020-11-18 20:46:59 +0530

    deploying

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,7 +1,28 @@
 import express from "express";
 const router = express.Router();
-import { authUser } from "../controller/userController.js";
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+} from "../controller/userController.js";
+import { protect, admin } from "../Middleware/authMiddleware.js";
 
 router.post("/login", authUser);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
+router.route("/").post(registerUser).get(protect, admin, getUsers);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,51 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import colors from "colors";
-import connectDB from "./config/db.js";
-import productRoutes from "./routes/productRoutes.js";
 import { notFound, errorHandler } from "./Middleware/errorMiddleware.js";
-import userRoutes from "./routes/userRoutes.js";
+import connectDB from "./config/db.js";
+import morgan from "morgan";
+import productRoutes from "./Routes/productRoutes.js";
+import userRoutes from "./Routes/userRoutes.js";
+import bodyParser from "body-parser";
+import orderRoutes from "./Routes/orderRoutes.js";
+import uploadRoutes from "./Routes/uploadRoutes.js";
+
 dotenv.config();
 
 connectDB();
 
 const app = express();
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
-
+app.use("/api/orders", orderRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/upload", uploadRoutes);
 
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 app.use(notFound);
 app.use(errorHandler);
 
